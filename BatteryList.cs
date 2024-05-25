721224705cd975b52726e82d7c90c45ce02e0f36
JSamuraiL <alex.ermiloff2017@yandex.ru>
2024-05-25 16:35:01 +0300

    Создание списка батарей

---
+++ BatteryList.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    public class BatteryList:List<Battery>
+    {
+        public void SortBatteries() 
+        {
+            var sortingbatteries = 
+                from batteries in this
+                orderby batteries.Capacity
+                select batteries;
+        }
+    }
+}
